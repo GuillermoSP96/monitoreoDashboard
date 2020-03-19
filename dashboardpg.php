ab110d5436a04609810dcc21d71c899ac96e77e5
Guillermo Sánchez <guillermo.sanchez@clearintelligence.mx>
2020-03-19 16:19:12 -0600

    first commit

---
+++ dashboardpg.php
@@ -0,0 +1,11 @@
+<?php
+/**
+* Plugin Name: Dashboard Monitoreo Pegalinas
+* Plugin URI: http://www.clearintelligence.mx/
+* Description: Plugin para el monitoreo del estado de los pedidos, apis y ventas
+* Version: 1.0.0
+* Author: Clear Intelligence - Guillermo Sanchez
+* Author URI: http://www.clearintelligence.mx
+*/
+
+?>
